2a6c2c9cfbe14115c3384a02b8aa92b96589143d
Ralitsa <ralitsa.simeonova933@gmail.com>
2018-04-24 16:25:40 +0300

    Update RunApp.java

--- students/RunApp.java
+++ students/RunApp.java
@@ -7,10 +7,26 @@ import java.util.ArrayList;
 public class RunApp {
 	
 	public static void main(String[] args) throws IOException {	
-		ArrayList<Student> studentList = Student.readStudentInfo();
-		
+		ArrayList<Student> studentList = Student.readStudentData();
+		Map<Long, Student> studentsMap = new HashMap<Long, Student>();
 		for (Student st : studentList) {
-			System.out.println(st.toString());
+			//System.out.println(st.toString());
+			studentsMap.put(st.getfNum(), st);
 		} 
+		
+	    // Getting a Set of Key-value pairs
+	    Set entrySet = studentsMap.entrySet();
+	 
+	    // Obtaining an iterator for the entry set
+	    Iterator it = entrySet.iterator();
+	 
+	    // Iterate through HashMap entries(Key-Value pairs)
+	    System.out.println("HashMap Key-Value Pairs : ");
+	    while(it.hasNext()){
+	       Map.Entry me = (Map.Entry)it.next();
+	       System.out.println("Key is: "+ me.getKey() + 
+	       " & " + 
+	       " value is: "+me.getValue());
+	   }
 	}
 }
