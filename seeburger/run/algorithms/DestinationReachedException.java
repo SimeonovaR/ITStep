4b2b5ae0588e915e2a543e274bfd347158725970
Ralitsa <ralitsa.simeonova933@gmail.com>
2018-05-30 11:19:39 +0300

    Add files via upload

---
+++ seeburger/run/algorithms/DestinationReachedException.java
@@ -0,0 +1,13 @@
+package com.seeburger.exceptions;
+
+public class DestinationReachedException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DestinationReachedException(String s) {
+		super(s);
+	}
+}
