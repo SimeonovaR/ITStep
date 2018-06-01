a94f3b40c162e8e1d3711f5f58133970e53a9769
Ralitsa <ralitsa.simeonova933@gmail.com>
2018-06-01 07:10:31 +0300

    Update RunDirectoryTransport.java

--- networking/RunDirectoryTransport.java
+++ networking/RunDirectoryTransport.java
@@ -1,13 +1,34 @@
 package com.seeburger.threads;
 
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
 
 public class RunDirectoryTransport {
 	public static void main(String[] args) throws IOException {
-		File source = new File("C:\\Users\\MKA\\Documents\\ralitsa");
-		File dest = new File("C:\\Users\\MKA\\Desktop");
-		DirectoryContentTransport.moveFiles(source, dest);
 
+		try {
+			Supervisor checkingForFiles = new Supervisor();
+			boolean flag = true;
+			while(flag) {	
+				Thread t1 = new Thread(checkingForFiles);  
+
+				t1.start();
+							
+				if (source.listFiles().length == 0) {
+					t1.wait(10000);
+				}
+				System.out.println("message = " + str);
+				if (str.equals("end"))	{
+					flag = false;
+				}
+
+			}
+			
+		} catch(Exception e) {
+			System.out.println(e);
+		} finally {
+			
+		}		
 	}
 }
