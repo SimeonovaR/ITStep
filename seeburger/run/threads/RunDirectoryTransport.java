3ef55b8aae5832a5f77482c631c45aad968fc83c
Ralitsa <ralitsa.simeonova933@gmail.com>
2018-06-14 23:37:09 +0300

    Update RunDirectoryTransport.java

--- seeburger/run/threads/RunDirectoryTransport.java
+++ seeburger/run/threads/RunDirectoryTransport.java
@@ -4,29 +4,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class RunDirectoryTransport {
-    private static boolean running = true;
-    private static Supervisor supervisorThread;
     
-	File source = new File("C:" + File.separator + "Users" + File.separator + "Ralitsa"  
-            + File.separator + "ralitsa");
-    File dest = new File("C:" + File.separator + "Users" + File.separator + "Ralitsa" 
-            + File.separator + "Desktop");
+	static File source = new File("D:" + File.separator + "proekt Front-end web");
+    static File dest = new File("C:" + File.separator + "Users" + File.separator + "Ralitsa" 
+            + File.separator + "Desktop" + File.separator + "ralitsa");
     
 	public static void main(String[] args) throws IOException {
-        supervisorThread = new Supervisor("supervisorThread");
-        supervisorThread.start();
-        
-	    try {
-			Supervisor checkingForFiles = new Supervisor("supervisorThread");
-			boolean flag = true;
-			while(running) {	
-				
-			}
-			
-		} catch(Exception e) {
-			System.out.println(e);
-		} finally {
-			
-		}		
+        DirectoryContentTransport transportThread = new DirectoryContentTransport(source, dest);
+        transportThread.start();
+        new Supervisor(transportThread).start();
 	}
 }
