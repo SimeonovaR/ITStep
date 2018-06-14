f578b1271482860fa4480786f643ee2930b11732
Ralitsa <ralitsa.simeonova933@gmail.com>
2018-06-14 23:38:18 +0300

    Update Supervisor.java

--- seeburger/run/threads/Supervisor.java
+++ seeburger/run/threads/Supervisor.java
@@ -3,33 +3,38 @@ package com.seeburger.threads;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.xml.transform.Source;
 
 public class Supervisor extends Thread {
-    private BufferedReader reader;
+    private static boolean running = true;
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    private boolean running = true;
-
-    public Supervisor(String name) {
-        super(name);
-        
-            
+    DirectoryContentTransport transport;
+    
+    public Supervisor(DirectoryContentTransport transport) {
+        this.transport = transport;          
     }
     
-
-    
-	@Override
-	public void run() {
-
-
-
-		try {
-			DirectoryContentTransport.reachBottomOfDirectory(source, dest);
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
+    @Override
+    public void run() {
+    	String consoleCommand = "";
+        try {
+            while (running) {
+            	if ((consoleCommand = reader.readLine()).equals("end")) {
+            		running = false;
+            	}
+            	else {
+                    try {
+						transport.reachBottomOfDirectory(transport.getSource().getAbsolutePath());
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+            	}
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
