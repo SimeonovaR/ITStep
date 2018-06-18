39d128062f93e6cd1f59a15156e8a6034b2f0ac4
Ralitsa <ralitsa.simeonova933@gmail.com>
2018-06-18 12:02:10 +0300

    Update RunDirectoryTransport.java

--- seeburger/run/threads/RunDirectoryTransport.java
+++ seeburger/run/threads/RunDirectoryTransport.java
@@ -1,17 +1,36 @@
 package com.seeburger.threads;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class RunDirectoryTransport {
+	
+    private static boolean running = true;
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     
-	static File source = new File("D:" + File.separator + "proekt Front-end web");
-    static File dest = new File("C:" + File.separator + "Users" + File.separator + "Ralitsa" 
+	static File source = new File("C:" + File.separator + "Users" + File.pathSeparator + "r.simeonova"
+			+ File.pathSeparator + "Documents" + File.pathSeparator + "New Folder");
+    static File dest = new File("C:" + File.separator + "Users" + File.separator + "r.simeonova" 
             + File.separator + "Desktop" + File.separator + "ralitsa");
     
 	public static void main(String[] args) throws IOException {
         DirectoryContentTransport transportThread = new DirectoryContentTransport(source, dest);
         transportThread.start();
-        new Supervisor(transportThread).start();
+    	try {
+            while (running) {
+            	if ((reader.readLine()).equals("end")) {
+            		running = false;
+            	}
+            	else {
+                    if(!transportThread.isEmptyDirectory()) {
+                    	transportThread.notify();
+                    }
+            	}
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 }
